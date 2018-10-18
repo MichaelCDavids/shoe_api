8917dd8c21a74a73cc20dfd136b49fb0ce76a49a
michael <michaelcdavids@gmail.com>
2018-10-18 16:25:37 +0200

    wrote tests for all factory functions and api calls

--- test/shoe-api-test.js
+++ test/shoe-api-test.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 const assert = require('assert');
 const shoeFactory = require('../services/shoes-factory');
 const cartFactory = require('../services/cart-factory');
@@ -91,15 +91,15 @@ describe('The Shoe Catalogue API Tests', function () {
             })
     });
     it('the addShoe function should return a status code of 200', function () {
-        let params = { 
-        "brand": "Nike",
-        "color": "Red",
-        "price": 1230,
-        "size": 4,
-        "in_stock": 48 
+        let params = {
+            "brand": "Nike",
+            "color": "Red",
+            "price": 1230,
+            "size": 4,
+            "in_stock": 48
         };
         request(app)
-            .post('/api/shoes/add',params)
+            .post('/api/shoes/add', params)
             .end(function (err, res) {
                 expect(res.statusCode).to.equal(200);
             })
@@ -131,70 +131,150 @@ describe('The Shoe Catalogue API Tests', function () {
 });
 
 describe('The Shoe Catalogue Factory Functions', function () {
+    beforeEach(async function () {
+        await pool.query('delete from shoes');
+        let shoeOne = {
+            "brand": "Nike",
+            "color": "Blue",
+            "price": 1000,
+            "size": 1,
+            "in_stock": 5
+        };
+        let shoeTwo = {
+            "brand": "Adidas",
+            "color": "White",
+            "price": 2000,
+            "size": 5,
+            "in_stock": 10
+        };
+        await shoeFactoryInstance.addStockItem(shoeOne);
+        await shoeFactoryInstance.addStockItem(shoeTwo);
+    });
     it('The get shoes function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
         let results = await shoeFactoryObject.getShoes();
-        assert.deepEqual(results, []);
+        results = [{
+                "brand_name": "Nike",
+                "color_name": "Blue",
+                "in_stock": 5,
+                "price": 1000,
+                "size": 1
+            },
+            {
+                "brand_name": "Adidas",
+                "color_name": "Blue",
+                "in_stock": 10,
+                "price": 2000,
+                "size": 5
+            }
+        ]
+        assert.deepEqual(results, [{
+                "brand_name": "Nike",
+                "color_name": "Blue",
+                "in_stock": 5,
+                "price": 1000,
+                "size": 1
+            },
+            {
+                "brand_name": "Adidas",
+                "color_name": "Blue",
+                "in_stock": 10,
+                "price": 2000,
+                "size": 5
+            }
+        ]);
     });
     it('The getBrands function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
         let results = await shoeFactoryObject.getBrands();
-        assert.deepEqual(results, []);
+        assert.deepEqual(results, [{
+            "brand_name": "Adidas",
+            "id": 2
+        }, {
+            "brand_name": "Nike",
+            "id": 1
+        }]);
     });
     it('The getColors function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
         let results = await shoeFactoryObject.getColors();
-        assert.deepEqual(results, []);
+        assert.deepEqual(results, [
+              {
+                "color_name": "Blue",
+                "id": 2
+              },
+              {
+                "color_name": "Red",
+                "id": 1
+              },
+              {
+                "color_name": "White",
+                "id": 3
+              }
+            ]);
     });
     it('The getSizes function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
         let results = await shoeFactoryObject.getSizes();
-        assert.deepEqual(results, []);
+        assert.deepEqual(results, [
+              {
+                "id": 2,
+                "size": 1
+              },
+              {
+                "id": 1,
+                "size": 4
+              },
+              {
+                "id": 3,
+                "size": 5
+              }
+            ]);
     });
     it('The filterBrand function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
         let results = await shoeFactoryObject.filterBrand(1);
-        assert.deepEqual(results, []);
+        assert.equal(results.length, 1);
     });
     it('The filterSize function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
-        let results = await shoeFactoryObject.filterSize(1);
-        assert.deepEqual(results, []);
+        let results = await shoeFactoryObject.filterSize(2);
+        assert.equal(results.length, 1);
     });
     it('The filterColor function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
-        let results = await shoeFactoryObject.filterColor(1);
-        assert.deepEqual(results, []);
+        let results = await shoeFactoryObject.filterColor(2);
+        assert.equal(results.length, 1);
     });
     it('The filterBrandSize function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
-        let results = await shoeFactoryObject.filterBrandSize(1,1);
-        assert.deepEqual(results, []);
+        let results = await shoeFactoryObject.filterBrandSize(1, 2);
+        assert.equal(results.length, 1);
     });
     it('The filterBrandColor function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
-        let results = await shoeFactoryObject.filterBrandColor(1,1);
-        assert.deepEqual(results, []);
+        let results = await shoeFactoryObject.filterBrandColor(1, 2);
+        assert.equal(results.length, 1);
     });
     it('The filterColorSize function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
-        let results = await shoeFactoryObject.filterColorSize(1,1);
-        assert.deepEqual(results, []);
+        let results = await shoeFactoryObject.filterColorSize(2, 2);
+        assert.equal(results.length, 1);
     });
     it('The filterBrandColorSize function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
-        let results = await shoeFactoryObject.filterBrandColorSize(1,1);
-        assert.deepEqual(results, []);
+        let results = await shoeFactoryObject.filterBrandColorSize(1, 2, 2);
+        assert.equal(results.length, 1);
     });
     it('The addStockItem function', async function () {
         let shoeFactoryObject = shoeFactoryInstance;
-        let params = { 
+        let params = {
             "brand": "Nike",
             "color": "Red",
             "price": 1230,
             "size": 4,
-            "in_stock": 48 
-            };
+            "in_stock": 48
+        };
         let results = await shoeFactoryObject.addStockItem(params);
         assert.deepEqual(results, "Shoe was added successfully!");
     });
